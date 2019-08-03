f516417279d1c22d8d58f54ea28f1c2d926c723b
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-03 11:30:32 +0100

    Refactored piano - instrument to extend stock - all tests passed

--- src/main/java/Instruments/Piano.java
+++ src/main/java/Instruments/Piano.java
@@ -1,10 +1,13 @@
 package Instruments;
 
-public class Piano extends Instrument {
+import Behaviours.ISell;
+
+public class Piano extends Instrument implements ISell {
     private int numKeys;
 
-    public Piano(String type, String colour, String material, int numKeys) {
-        super(type, colour, material);
+    public Piano(String description, double buyFor, double sellFor,
+                 String type, String colour, String material, int numKeys) {
+        super(description, buyFor, sellFor, type, colour, material);
         this.numKeys = numKeys;
     }
 
--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -10,7 +10,23 @@ public class PianoTest {
 
     @Before
     public void setup() {
-        piano = new Piano("Classical","Deep brown", "Oak", 88);
+        piano = new Piano("Looks great in the corner",500.00,650.00,
+                "Classical","Deep brown", "Oak", 88);
+    }
+
+    @Test
+    public void canGetDescription() {
+        assertEquals("Looks great in the corner", piano.getDescription());
+    }
+
+    @Test
+    public void canGetBuyFor() {
+        assertEquals(500.00, piano.getBuyFor(), 0.01);
+    }
+
+    @Test
+    public void canGetSellFor() {
+        assertEquals(650.00, piano.getSellFor(), 0.01);
     }
 
     @Test
@@ -29,12 +45,20 @@ public class PianoTest {
     }
 
     @Test
+    public void canGetKeys() {
+        assertEquals(88, piano.getNumKeys());
+    }
+
+    @Test
     public void canPlay() {
         assertEquals("Plink Plink", piano.play());
     }
 
     @Test
-    public void canGetKeys() {
-        assertEquals(88, piano.getNumKeys());
+    public void canCalculateMarkup() {
+        assertEquals(150.00, piano.calculateMarkup(), 0.01);
     }
+
+
+
 }

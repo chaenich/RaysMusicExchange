d73d96dbc06b92f4a9bd37d5f4b4992fcfdbe0d4
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-03 11:57:14 +0100

    Refactored all accessory related files to ensure ISell only implemented in stock (top level) and removed any unecessary imports. All tests passed.

--- src/main/java/Accessories/Drumstick.java
+++ src/main/java/Accessories/Drumstick.java
@@ -1,7 +1,5 @@
 package Accessories;
 
-import Behaviours.ISell;
-
 public class Drumstick extends Accessory {
 
     public Drumstick(String description, double buyFor, double sellFor, String category) {
--- src/main/java/Accessories/Sheet.java
+++ src/main/java/Accessories/Sheet.java
@@ -1,7 +1,5 @@
 package Accessories;
 
-import Behaviours.ISell;
-
 public class Sheet extends Accessory {
 
     public Sheet(String description, double buyFor, double sellFor, String category) {

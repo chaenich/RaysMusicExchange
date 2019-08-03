e63dac18be4e59b09997126d0346c623fce69cad
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-03 11:33:37 +0100

    Removed unnecessary private fields

--- src/main/java/Instruments/Flute.java
+++ src/main/java/Instruments/Flute.java
@@ -4,9 +4,6 @@ import Behaviours.ISell;
 
 public class Flute extends Instrument implements ISell {
     private int numValves;
-    private String description;
-    private double buyFor;
-    private double sellFor;
 
     public Flute(String description, double buyFor, double sellFor,
                  String type, String colour, String material, int numValves) {
